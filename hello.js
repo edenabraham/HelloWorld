9ba9a209bcefdb057c3cb2679012d16b8883de0a
Eden Abraham <eabraham@atl.yearup.org>
2022-09-27 14:55:22 -0400

    Page 42

---
+++ hello.js
@@ -0,0 +1,68 @@
+// Hello World
+
+"use strict";
+
+var message = "Hello World";
+console.log(message);
+
+//pg42 Excercise 1
+var customerId= "John";
+var customerFirst, customerMiddle, customerLast;
+customerFirst= "John";
+customerMiddle= "Jim";
+customerLast= "Smith";
+var customerGender= "Male";
+var customerDOB= "04/26/1989";
+var customerDriverLicenseNumber= "65784537"
+var customerAutoPolicyNumber= "123-4567-8910"
+
+//pg42 Excercise 2
+//Words you cant use for variables//
+for
+char
+break
+debugger
+finally
+goto
+false
+delete
+eval
+function
+
+//words you cant use for variables that are Javascript//
+    //built-in objects, properties, and methods//
+array
+hasOwnProperty
+isPropertyOf
+string
+name
+infinity
+Number
+toString
+NaN
+object
+
+//HMTL and Window object names that you cant use as variables//
+alert
+all
+assign
+clearInterval
+closed
+element
+crypto
+embeds
+link
+location
+
+//HTML event names you should not use for varible names//
+onblur
+onkeydown
+onload
+onlcick
+onmouseup
+onmouseover
+
+//pg 42 Excercise 3
+var myName="Eden";
+var myHomeCity= "Stone Mountain";
+var myHomeState= "Georgia";
